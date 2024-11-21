9651a76be09e0c548120b4e273583ee082b27991
Alban Billiette <albillie@c1r5p6.42lehavre.fr>
2024-11-21 01:19:10 +0100

    UPDATE

--- src/parser.c
+++ src/parser.c
@@ -6,31 +6,31 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:10:51 by albillie          #+#    #+#             */
-/*   Updated: 2024/11/19 07:50:33 by albillie         ###   ########.fr       */
+/*   Updated: 2024/11/21 01:17:52 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void	check_if_map(char *filename, int argc)
+bool	check_extension(char *filename, char *extension)
+{
+
+
+
+}
+
+void	map_checker(char *filename, int argc)
 {
 	int fd;
 
 	if (argc < 2)
-	{
-		printf("Error\nPlease enter map name !\n"), exit(EXIT_FAILURE);
-	}
+		ft_printf("Error\nPlease enter map name !\n"), exit(EXIT_FAILURE);
 	else if (argc > 2)
-	{
-		printf("Error\nYou have too much arguments !\n"), exit(EXIT_FAILURE);
-	}
+		ft_printf("Error\nYou have too much arguments !\n"), exit(EXIT_FAILURE);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-	{
-		printf("Error\nInvalid map name !\n");
-		exit(EXIT_FAILURE);
-	}
-	printf("Map (%s) opened!\n", filename);
+		ft_printf("Error\nInvalid map name !\n"), exit(EXIT_FAILURE);
+	ft_printf("Map (%s) opened!\n", filename);
 }
 
 int	count_lines(char *filename)
@@ -51,26 +51,16 @@ int	count_lines(char *filename)
 	close(fd);
 	return(lines);
 }
-bool	is_map_valid(char *filename)
+/* bool	is_map_valid(char *filename)
 {
-	int fd;
-	char *buffer = NULL;
-	fd = open(filename, O_RDONLY);
-	int i = 0;
-
-	while (!ft_strchr(buffer, '\n'))
-	{
-		read(fd, buffer, 1);
-		i++;
-	}
-	printf("%d", i);
-	return true;
-}
+	open(filename, O_RDONLY);
+} */
 
 
 
 int main(int argc, char **argv)
 {
 	check_if_map(argv[1], argc);
-	is_map_valid(argv[1]);
+	ft_printf("%d", count_lines(argv[1]));
 }
+
